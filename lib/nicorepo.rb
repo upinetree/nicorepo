0f8d6c263039cc8a6ddf026a5dcac7851a97776a
upinetree <essequake@gmail.com>
2013-06-23 15:37:28 +0900

    move 'Log' class to new log.rb

--- lib/nicorepo.rb
+++ lib/nicorepo.rb
@@ -1,60 +1,11 @@
 require 'mechanize'
+require_relative 'nicorepo/log.rb'
 
 class Nicorepo
 
   module URL
     LOGIN = 'https://secure.nicovideo.jp/secure/login?site=niconico'
     REPO_ALL = 'http://rd.nicovideo.jp/cc/my/zerotopall'
-  end
-
-  class Log
-    attr_accessor :body, :target, :url, :author, :kind, :date
-
-    def initialize(node = nil)
-      if node.nil? then return end
-      @body   = parse_body   node
-      @target = parse_target node
-      @url    = parse_url    node
-      @author = parse_author node
-      @kind   = parse_kind   node
-      @date   = parse_date   node
-    end
-
-    private
-
-    def parse_body(node)
-      node.search('div.log-body').first.inner_text.gsub(/(\t|\r|\n)/, "")
-    end
-
-    def parse_target(node)
-      node.search('div.log-target-info/a').first.inner_text
-    end
-
-    def parse_url(node)
-      node.search('div.log-target-info/a').first['href']
-    end
-
-    def parse_author(node)
-      node.search('div.log-body/a').first.inner_text
-    end
-
-    # example: 'log.log-community-video-upload' -> 'community-video-upload'
-    def parse_kind(node)
-      cls = node['class']
-      trim = 'log-'
-
-      index = cls.index(/#{trim}/)
-      return cls if index.nil?
-
-      index += trim.size
-      cls[index..cls.size]
-    end
-
-    def parse_date(node)
-      d = node.search('div.log-footer/a.log-footer-date/time').first['datetime']
-      Time.xmlschema(d).localtime
-    end
- 
   end
 
   class LoginError < StandardError; end
