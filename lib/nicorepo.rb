33213451b051c20be2c19a660119d600cb330c94
upinetree <t.matsumoto@everyleaf.com>
2014-04-29 21:24:40 +0900

    Move reports interface to Nicorepo

--- lib/nicorepo.rb
+++ lib/nicorepo.rb
@@ -1,28 +1,36 @@
-require 'forwardable'
 require 'mechanize'
 require 'nicorepo/reports'
 
 class Nicorepo
-  extend Forwardable
-
   class LoginError < StandardError; end
 
+  PER_PAGE = 20
   LOGIN_URL = 'https://secure.nicovideo.jp/secure/login?site=niconico'
 
   attr_reader :agent
-
-  def_delegators :@reports, :all, :videos, :lives
 
   def initialize
     @agent = Mechanize.new
     @agent.ssl_version = 'SSLv3'
     @agent.request_headers = { 'accept-language' => 'ja-JP', 'content-language' => 'ja-JP' }
-    @reports = Reports.new(@agent)
   end
 
   def login(mail, pass)
     page = @agent.post(LOGIN_URL, mail: mail, password: pass)
     raise LoginError, "Failed to login" if page.header["x-niconico-authflag"] == '0'
+  end
+
+  def all(req_num = PER_PAGE)
+    page_nest_max = req_num / PER_PAGE + 1
+    Reports.new(@agent).fetch(req_num, page_nest_max)
+  end
+
+  def videos(req_num = 3, page_nest_max = 5)
+    VideoReports.new(@agent).fetch(req_num, page_nest_max)
+  end
+
+  def lives(req_num = 3, page_nest_max = 5)
+    LiveReports.new(@agent).fetch(req_num, page_nest_max)
   end
 end
 
--- lib/nicorepo/reports.rb
+++ lib/nicorepo/reports.rb
@@ -1,36 +1,33 @@
 require 'nicorepo/report'
+require 'forwardable'
 
 class Nicorepo
   class Reports
+    extend Forwardable
+
     class ReportsAccessError < StandardError; end
 
     TOP_URL = 'http://www.nicovideo.jp/my/top'
-    PER_PAGE = 20
+
+    attr_reader :reports
+    def_delegators :@reports, :size
 
     def initialize(agent)
       @agent = agent
+      @reports = []
     end
 
-    def all(req_num = PER_PAGE)
-      page_nest_max = req_num / PER_PAGE + 1
-      fetch(req_num, page_nest_max)
+    def fetch(req_num, page_nest_max)
+      @reports = fetch_recursively(req_num, page_nest_max)
     end
 
-    def videos(req_num = 3, page_nest_max = 5)
-      filtered_by('video-upload', req_num, page_nest_max)
-    end
-
-    def lives(req_num = 3, page_nest_max = 5)
-      filtered_by('live', req_num, page_nest_max)
-    end
-
-    def filtered_by(filter, req_num = PER_PAGE, page_nest_max = 1)
-      fetch(req_num, page_nest_max, filter)
+    def fetch_with_filtere(filter, req_num, page_nest_max)
+      @reports = fetch_recursively(req_num, page_nest_max, filter)
     end
 
     private
 
-    def fetch(req_num, page_nest_max, filter = nil, url = TOP_URL)
+    def fetch_recursively(req_num, page_nest_max, filter = nil, url = TOP_URL)
       return [] unless page_nest_max > 0
 
       # fetch current reports
@@ -50,7 +47,7 @@ class Nicorepo
       if reports.size < req_num then
         begin
           next_url = page.search('a.next-page-link').first['href']
-          next_reports = fetch(req_num - reports.size, page_nest_max - 1, filter, next_url)
+          next_reports = fetch_recursively(req_num - reports.size, page_nest_max - 1, filter, next_url)
         rescue
           return reports
         else
@@ -63,6 +60,18 @@ class Nicorepo
 
     def report_nodes(page)
       page.search('div.timeline/div.log')
+    end
+  end
+
+  class VideoReports < Reports
+    def fetch(req_num, page_nest_max)
+      fetch_with_filtere('video-upload', req_num, page_nest_max)
+    end
+  end
+
+  class LiveReports < Reports
+    def fetch(req_num, page_nest_max)
+      fetch_with_filtere('live', req_num, page_nest_max)
     end
   end
 end
