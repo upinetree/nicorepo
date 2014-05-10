d971f8e4a63b8965a054d3b63c84caa8c8b607bc
upinetree <t.matsumoto@everyleaf.com>
2014-05-11 01:31:17 +0900

    Add cli command options 'hours' and 'days'

--- lib/nicorepo/cli/cli.rb
+++ lib/nicorepo/cli/cli.rb
@@ -62,9 +62,11 @@ class Nicorepo
       desc "all", "fetch all reports"
       option :"request-num", type: :numeric, aliases: :n
       option :"latest", type: :boolean, aliases: :l
+      option :"days", type: :numeric, aliases: :d
+      option :"hours", type: :numeric, aliases: :h
       def all
         request_num = options[:"request-num"] || conf.request_num("all")
-        request_options = options[:latest] ? { since: cached_at } : { since: nil }
+        request_options = { since: parse_since_options(options) }
         cache(repo.all(request_num, request_options))
         show
       end
@@ -73,10 +75,12 @@ class Nicorepo
       option :"request-num", type: :numeric, aliases: :n
       option :"limit-page", type: :numeric, aliases: :p
       option :"latest", type: :boolean, aliases: :l
+      option :"days", type: :numeric, aliases: :d
+      option :"hours", type: :numeric, aliases: :h
       def videos
         request_num = options[:"request-num"] || conf.request_num("videos")
         limit_page  = options[:"limit-page"]  || conf.limit_page("videos")
-        request_options = options[:latest] ? { since: cached_at } : { since: nil }
+        request_options = { since: parse_since_options(options) }
         cache(repo.videos(request_num, limit_page, request_options))
         show
       end
@@ -85,10 +89,12 @@ class Nicorepo
       option :"request-num", type: :numeric, aliases: :n
       option :"limit-page", type: :numeric, aliases: :p
       option :"latest", type: :boolean, aliases: :l
+      option :"days", type: :numeric, aliases: :d
+      option :"hours", type: :numeric, aliases: :h
       def lives
         request_num = options[:"request-num"] || conf.request_num("lives")
         limit_page  = options[:"limit-page"]  || conf.limit_page("lives")
-        request_options = options[:latest] ? { since: cached_at } : { since: nil }
+        request_options = { since: parse_since_options(options) }
         cache(repo.lives(request_num, limit_page, request_options))
         show
       end
@@ -141,6 +147,19 @@ class Nicorepo
         cached_reports.unshift(reports).flatten!
         self.class.cache[:recent_tail] = reports.size
         self.class.cache[:cached_at] = Time.now
+      end
+
+      def parse_since_options(options)
+        case
+        when options[:latest]
+          cached_at
+        when options[:days]
+          Time.now - options[:days] * 24 * 60 * 60
+        when options[:hours]
+          Time.now - options[:hours] * 60 * 60
+        else
+          nil
+        end
       end
 
       def open_numbered_link(request_num)
